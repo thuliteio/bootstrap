9ac7f8e62d8d05ae836cb7103f02e6e5c6d35d84
Henk Verlinde <henk@henkverlinde.com>
2024-04-19 13:42:52 +0200

    Update for new Hyas setup

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -9,7 +9,6 @@
 import { Dropdown, Offcanvas, Popover } from 'bootstrap';
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+    new Popover(popover);
+});
